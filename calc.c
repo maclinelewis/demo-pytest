b117e6fed57ead74680e509eef4996879a9551b1
maclinelewis <47381672+maclinelewis@users.noreply.github.com>
2020-09-21 15:35:49 +0530

    Add files via upload

---
+++ calc.c
@@ -0,0 +1,20 @@
+#include "calc.h"
+int add_c(int x, int y)
+{
+	return x+y;
+}
+
+int sub_c(int x, int y)
+{
+	return x-y;
+}
+
+float div_c (int x, int y)
+{
+	return ( (x + 0.0)/y);
+}
+
+int mul_c (int x, int y)
+{
+	return x*y;
+}
